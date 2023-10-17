afadcb0d1c0b3767274af3804326dccb125082e7
EndoGaming <100874097+EndoGaming@users.noreply.github.com>
2023-10-17 20:12:32 +0200

    Add files via upload

---
+++ locker.py
@@ -0,0 +1,108 @@
+import pyautogui
+import os
+import time
+
+with open("agent_names.txt", "r") as file:
+    list = file.readlines()
+for i in range(len(list)):
+    for n in range(len(list[i])):
+        if list[i][n] == "\n":
+            list[i] = list[i][:n]
+            break
+
+with open("box_coords.txt", "r") as file:
+    box_coords = file.readlines()
+for i in range(len(box_coords)):
+    for n in range(len(box_coords[i])):
+        if box_coords[i][n] == "\n":
+            box_coords[i] = int(box_coords[i][:n])
+            break
+
+coords_list = []
+try:
+    with open("coords.txt", "r") as file:
+        for line in file:
+            coords_list.append(line)
+except:
+        print("Setup was not completed!")
+        print("Please setup correctly before usage!")
+        time.sleep(3)
+        exit()
+
+agent_list = {}
+for i in range(len(coords_list)):
+    whole_coord = coords_list[i]
+    if whole_coord == "Not found\n" or whole_coord == "None\n":
+        continue
+    final_coord = []
+    index_x1 = 9
+    index_x2 = 0
+    for n in range(len(whole_coord)):
+        if whole_coord[n] == ",":
+            index_x2 = n
+            break
+    index_y1 = index_x2 + 6
+    for n in range(len(whole_coord)):
+        if whole_coord[index_y1 + n] == ",":
+            index_y2 = (index_y1 + n)
+            break
+    index_z1 = index_y2 + 8
+    for n in range(len(whole_coord)):
+        if whole_coord[index_z1 + n] == ",":
+            index_z2 = (index_z1 + n)
+            break
+    index_w1 = index_z2 + 9
+    for n in range(len(whole_coord)):
+        if whole_coord[index_w1 + n] == ")":
+            index_w2 = (index_w1 + n)
+            break
+    final_coord.append(whole_coord[index_x1:index_x2])
+    final_coord.append(whole_coord[index_y1:index_y2])
+    final_coord.append(whole_coord[index_z1:index_z2])
+    final_coord.append(whole_coord[index_w1:index_w2])
+    agent_list[list[i]] = final_coord
+
+def start_menu():
+    if agent_list == {}:
+        print("Setup was not completed!")
+        print("No Agents found!")
+        time.sleep(3)
+        exit()
+    full_list = "List with all available agents: "
+    for name in agent_list:
+        full_list = full_list + name.title() + ", "
+    full_list = full_list[0:-2]
+    print(full_list + "\n")
+    agent = input("Choose Agent: ")
+    return agent.lower()
+
+agent_selected = start_menu()
+
+while True:
+    if agent_selected not in agent_list:
+        os.system("cls")
+        print(f"{agent_selected} is invalid!\n")
+        agent_selected = start_menu()
+    else:
+        if agent_selected == "kayo":
+            os.system("cls")
+            print("You've chosen KAY/O!\n")
+            print("Program running...")
+            break
+        else:
+            os.system("cls")  
+            print("You've chosen " + agent_selected.title() + "!\n")
+            print("Program running...")
+            break
+try:
+    while True:
+        if pyautogui.locateOnScreen(agent_selected + ".png", confidence= 0.9, region=(int(agent_list[agent_selected][0]), int(agent_list[agent_selected][1]), int(agent_list[agent_selected][2]), int(agent_list[agent_selected][3]))):
+            location_x = int(agent_list[agent_selected][0]) + 40 # Muss maybe angepasst werden bei Änderungen am Gamedesign
+            location_y = int(agent_list[agent_selected][1]) + 35 # Muss maybe angepasst werden bei Änderungen am Gamedesign
+            pyautogui.click(location_x, location_y)
+            pyautogui.moveTo(box_coords[0], box_coords[1], 0.001)
+            pyautogui.click()
+except KeyboardInterrupt:
+    print("\nProgram stopped!\n")
+except Exception as err:
+    print(err)
---
+++ tracker_for_locker.py
@@ -0,0 +1,32 @@
+import pyautogui
+import time
+with open("agent_names.txt", "r") as file:
+    agent_list = file.readlines()
+for i in range(len(agent_list)):
+    for n in range(len(agent_list[i])):
+        if agent_list[i][n] == "\n":
+            agent_list[i] = agent_list[i][:n]
+            break
+
+timer = 3
+while True:   
+    print(f"Starts in {timer} seconds!")
+    time.sleep(1)
+    timer -= 1
+    if timer == 0:
+        break
+
+coordinates = []
+for agent_selected in agent_list:
+    try:
+        location = pyautogui.locateOnScreen(agent_selected + ".png", confidence= 0.9)
+        print(agent_selected.title() + " found!")
+        coordinates.append(location)
+    except:
+        coordinates.append("Not found")
+        print(agent_selected.title() + " not found!")
+        continue
+
+with open("coords.txt", 'w') as final_coords:
+   for i in range(len(agent_list)):
+       final_coords.write(f"{coordinates[i]}\n")
